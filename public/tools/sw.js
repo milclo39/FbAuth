f245b03efae6009d12db530d66acb4b0b882e317
wood-horse\milclo39 <e@e>
2026-06-13 23:19:55 +0900

    音楽プレイヤーの修正。リソースはmicloboxを参照する

---
+++ public/tools/sw.js
@@ -0,0 +1,28 @@
+var CACHE_NAME = 'paint-app-v1';
+var ASSETS = [
+	'/',
+	'/index.html',
+	'/main.html',
+	'/mplayer.html',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(ASSETS);
+			})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
+});
